042b155c8fab8ed7c6680c1bfdfca62c98ddf636
franck-benault <franck-benault@orange.fr>
2013-08-07 22:23:47 +0200

    add sample1

--- sample1/src/main/java/net/franckbenault/iterator/sample1/Sample1.java
+++ sample1/src/main/java/net/franckbenault/iterator/sample1/Sample1.java
@@ -1,5 +1,37 @@
 package net.franckbenault.iterator.sample1;
 
-public class Sample1 {
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Sample1 implements Iterator<String> {
+	
+	private Set<String> mySet = new HashSet<String>();
+	private Iterator<String> iterator;
+	
+	public Sample1() {
+		mySet.add("val1");
+		mySet.add("val2");
+		mySet.add("val3");
+		
+		iterator = mySet.iterator();
+	}
+
+	@Override
+	public boolean hasNext() {
+		return iterator.hasNext();
+	}
+
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return iterator.next();
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
  //comment
 }
---
+++ sample1/src/test/java/net/franckbenault/iterator/sample1/Sample1Test.java
@@ -0,0 +1,41 @@
+package net.franckbenault.iterator.sample1;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Sample1Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Sample1 sample = new Sample1();
+		int counter = 0;
+		
+		while(sample.hasNext()) {
+			assertNotNull(sample.next());
+			counter++;
+		}
+		assertEquals(counter,3L);
+	}
+
+}
