f155470a19d42d0fb248214534ee84cf700b7080
franck-benault <franck-benault@orange.fr>
2013-08-20 23:17:55 +0200

    sample4 complete

---
+++ sample4/src/main/java/net/franckbenault/iterator/Person.java
@@ -0,0 +1,71 @@
+package net.franckbenault.iterator;
+
+import org.apache.log4j.Logger;
+
+public class Person {
+	
+	private static final Logger logger = Logger.getLogger(Person.class);
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private Integer age;
+	
+	public Person(String line) {
+		String tab[] = null;
+		if(null!=line)
+			tab = line.split(":");
+		
+		if(tab!=null)
+			firstName=tab[0];
+		if(tab!=null && tab.length>=1)
+			lastName=tab[1];
+		if(tab!=null && tab.length>=2)
+			try {
+				age=Integer.valueOf(tab[2]);
+			} catch (java.lang.NumberFormatException e) {
+				logger.error(e);
+			}
+			
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+	
+	public boolean check() {
+		if(age==null)
+			return false;
+		
+		if(age<0)
+			return false;
+		
+		if(age>122)
+			return false;
+			
+		if (firstName==null)
+			return false;
+
+		if (lastName==null)
+			return false;
+		
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		
+		return firstName +" "+lastName+" " +age;
+	}
+
+}
---
+++ sample4/src/test/java/net/franckbenault/iterator/PersonTest.java
@@ -0,0 +1,67 @@
+package net.franckbenault.iterator;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PersonTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+	@Test
+	public void testPerson1()  {
+		Person person =new Person("AA:BB:34");
+		assertNotNull(person);
+		assertTrue(person.check());
+	}
+	
+	@Test
+	public void testPersonNullInput()  {
+		Person person =new Person(null);
+		assertNotNull(person);
+		assertTrue(!person.check());
+	}
+	
+	@Test
+	public void testPersonWrongAgeNull()  {
+		Person person =new Person("AA:BB:E4");
+		assertNotNull(person);
+		assertNull(person.getAge());
+		assertTrue(!person.check());
+	}
+	
+	@Test
+	public void testPersonWrongAgeNegative()  {
+		Person person =new Person("AA:BB:-4");
+		assertNotNull(person);
+		assertNotNull(person.getAge());
+		assertTrue(!person.check());
+	}
+	
+	@Test
+	public void testPersonWrongAgeTooOld()  {
+		Person person =new Person("AA:BB:140");
+		assertNotNull(person);
+		assertNotNull(person.getAge());
+		assertTrue(!person.check());
+	}
+}
