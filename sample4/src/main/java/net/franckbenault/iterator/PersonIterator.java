9cd331f44ea5e760f25b6ca3eb748519df94f6fc
franck-benault <franck-benault@orange.fr>
2013-08-21 23:00:25 +0200

    sample4 add files and iterator

---
+++ sample4/src/main/java/net/franckbenault/iterator/PersonIterator.java
@@ -0,0 +1,81 @@
+package net.franckbenault.iterator;
+
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+
+public class PersonIterator implements Iterator<Person> {
+	
+	private static final Logger logger = Logger.getLogger(PersonIterator.class);
+	
+
+	private Person person;
+	private StringIterator stringIterator;
+	
+	public PersonIterator(String fileNameInput) throws IOException {
+		logger.info("open file "+fileNameInput);
+		stringIterator = new StringIterator(fileNameInput);
+	    
+	    person = getNextValidPerson(); 
+	    
+	}
+
+	@Override
+	public boolean hasNext() {
+		return person!=null;
+	}
+
+	@Override
+	public Person next() {
+		Person tempPerson = person;
+		
+		person = getNextValidPerson(); 
+
+		return tempPerson;
+	}
+	
+	
+
+	
+	private Person getNextValidPerson() {
+		String line = stringIterator.next();
+		boolean isValid = false;
+		Person tempPerson =null;
+		//return only valid person
+		Person res = null;
+		
+		if(line!=null) {
+			tempPerson = new Person(line);
+			isValid = tempPerson.check();
+			if(isValid)
+				res = tempPerson;
+		}
+		
+		
+		while(!isValid && line!=null) {
+			line = stringIterator.next();
+			if(line!=null) {
+				tempPerson = new Person(line);
+				isValid = tempPerson.check();
+				if(isValid)
+					res = tempPerson;
+			}
+		}
+		logger.info("tempPerson "+tempPerson+" "+isValid);
+		return res;
+	}
+	
+	
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException(); 		
+	}
+	
+	
+	public void close() {
+		stringIterator.close();
+	}
+
+}
---
+++ sample4/src/test/java/net/franckbenault/iterator/PersonIteratorTest.java
@@ -0,0 +1,104 @@
+package net.franckbenault.iterator;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PersonIteratorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/*
+	 * test with File1 no comment
+	 */
+	@Test
+	public void testWithFile1() throws IOException  {
+		PersonIterator iterator = null;
+		try {
+			iterator = new PersonIterator("../sample3/src/main/resources/file1.txt");
+			int counter = 0;
+			
+			while(iterator.hasNext()) {
+				assertNotNull(iterator.next());
+				counter++;
+			}
+			assertEquals(counter,4L);
+		} finally {
+			iterator.close();
+		}
+		
+	}
+	
+	/*
+	 * test with file2
+	 * file with several lines in comment
+	 */
+	@Test
+	public void testWithFile2() throws IOException  {
+		PersonIterator iterator = null;
+		try {
+			iterator = new PersonIterator("../sample3/src/main/resources/file2.txt");
+			int counter = 0;
+			
+			while(iterator.hasNext()) {
+				Person person = iterator.next();
+				System.out.println(person);
+				assertNotNull(person);
+				counter++;
+			}
+			assertEquals(counter,1L);
+		} finally {
+			iterator.close();
+		}	
+	}
+
+	/*
+	 * test with file3
+	 * file with all lines in comment
+	 */
+	@Test
+	public void testWithFile3() throws IOException  {
+		PersonIterator iterator = null;
+		try {
+			iterator = new PersonIterator("../sample3/src/main/resources/file3.txt");
+			int counter = 0;
+			
+			while(iterator.hasNext()) {
+				assertNotNull(iterator.next());
+				counter++;
+			}
+			assertEquals(counter,0L);
+		} finally {
+			iterator.close();
+		}	
+	}
+	
+	@Test(expected=IOException.class)
+	public void testWithWrongFile() throws IOException  {
+		
+		new PersonIterator("WrongFileName");
+
+
+		
+	}
+}
